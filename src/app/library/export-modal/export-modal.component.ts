93c3fc726f299a73bdbf814d0773b62e9ae29e34
James Hayward <hayward.jp@gmail.com>
2020-01-22 18:53:02 +0000

    working copy function

--- src/app/library/export-modal/export-modal.component.ts
+++ src/app/library/export-modal/export-modal.component.ts
@@ -8,10 +8,20 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 })
 
 export class ExportModalComponent {
+  private text: string;
 
   constructor(
     public dialogRef: MatDialogRef<ExportModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
-    ) {}
+    ) {
+      this.text = this.data.join('\n\n');
+    }
+
+    copyText() {
+      const text: any = document.querySelector('#ref-area');
+      text.select();
+      document.execCommand('copy');
+      this.dialogRef.close();
+    }
 
 }
